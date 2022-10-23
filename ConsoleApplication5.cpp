42f57093cb7cb847c3dc2dfb97620e3ab6bac476
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-10-23 11:31:31 +0300

    Add files via upload

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,20 @@
+﻿// ConsoleApplication5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A;
+    cin >> A;
+    while (A > 0)
+    {
+        cout << A % 10;
+        A /= 10;
+    }
+    cout << endl;
+    if (A == 0) cout << "0";
+}
+
+
